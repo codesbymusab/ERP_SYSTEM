0fc98fc375ece243fa1ca31d105aab0e00cb6515
shayanO-O <shayanahmed670@gmail.com>
2025-11-30 14:41:01 +0500

    Flicker Resolution - Double Buffering

--- GUI/Forms/MainPageForm.cs
+++ GUI/Forms/MainPageForm.cs
@@ -10,18 +10,32 @@ using System.Windows.Forms;
 using System.Windows.Media.Animation;
 
 namespace ERP_System
-{   
-   
+{
     public partial class MainPageForm : Form
     {
-        
         public MainPageForm()
         {
             InitializeComponent();
 
+            // ADDED: Enable double buffering to reduce flickering
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                         ControlStyles.AllPaintingInWmPaint |
+                         ControlStyles.UserPaint, true);
+            this.UpdateStyles();
+
+            // Enable double buffering for bunifuPages if possible
+            if (this.bunifuPages1 != null)
+            {
+                typeof(Control).GetProperty("DoubleBuffered",
+                    System.Reflection.BindingFlags.NonPublic |
+                    System.Reflection.BindingFlags.Instance)
+                    ?.SetValue(this.bunifuPages1, true, null);
+            }
+
             // Set the reference to this form in the SideNavBar
             this.sideNavBar1.SetMainPageForm(this);
-          
+
             // Attach event handlers for navigation
             this.sideNavBar1.btnDashboard.Click += dashboardBtn_Click;
             this.sideNavBar1.btnProd.Click += productsBtn_Click;
@@ -34,49 +48,62 @@ namespace ERP_System
             this.sideNavBar1.btnUserProfile.Click += profileBtn_Click;
         }
 
+        private void ChangePage(int pageIndex)
+        {
+            this.SuspendLayout();
+            try
+            {
+                this.bunifuPages1.PageIndex = pageIndex;
+            }
+            finally
+            {
+                this.ResumeLayout(false);
+            }
+        }
+
         private void dashboardBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 0;
+            ChangePage(0);
         }
 
         private void employBtn_Click(object sender, EventArgs e)
-        {   
-            this.bunifuPages1.PageIndex = 1;
+        {
+            ChangePage(1);
         }
 
         private void productsBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 2;
+            ChangePage(2);
         }
 
         private void stkAdjBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 3;
+            ChangePage(3);
         }
 
         private void suppliersBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 4;
+            ChangePage(4);
         }
 
         private void historyBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 5;
+            ChangePage(5);
         }
 
         private void purchasesBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 6;
+            ChangePage(6);
         }
 
         private void salesBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 7;
+            ChangePage(7);
         }
 
         private void profileBtn_Click(object sender, EventArgs e)
         {
-            this.bunifuPages1.PageIndex = 8;
+            ChangePage(8);
         }
 
         private void EmployPage_Click(object sender, EventArgs e)
