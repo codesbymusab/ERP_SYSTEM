f69e8cd2449f6cfe078713783f87a0db986eeeab
shayanO-O <shayanahmed670@gmail.com>
2025-11-30 17:01:31 +0500

    Added Logo in Login Form

--- GUI/Forms/LoginForm.Designer.cs
+++ GUI/Forms/LoginForm.Designer.cs
@@ -508,6 +508,7 @@
             this.ClientSize = new System.Drawing.Size(1902, 1032);
             this.Controls.Add(this.pnlMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginForm";
